0ad2484c3c0a63f1f20a61abd55992eec89dcc32
Leonardo Vivanco <leonardo.vivanco@wetek.com>
2021-03-20 18:38:21 +0000

    Add Card

---
+++ src/assets/js/CardHtml.js
@@ -0,0 +1,14 @@
+const CardHtml = ({ id, image_url, name, abv, ibu }) => `
+       <div class='card' data-id="${id}">
+         <div class='beer'>
+          <img class='beer__img' src="${image_url}" />
+          <h3 class='beer__name'>${name}</h3>
+         </div>
+         <div class='beer__info'>
+          <div class="beer__abv"><span>${abv}%</span></div>
+          <div class="beer__ibu">IBU: ${ibu}</div>
+         </div>
+     </div>
+    `
+
+export default CardHtml
