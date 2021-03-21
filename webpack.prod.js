cb0add34ce99a3424c87a4e128a4b6fdbb3c8099
Leonardo Vivanco <leonardo.vivanco@wetek.com>
2021-03-21 16:03:18 +0000

    Add favicon

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,24 +1,25 @@
-const path = require("path");
-const common = require("./webpack.common");
-const merge = require("webpack-merge");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
-var HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path')
+const common = require('./webpack.common')
+const merge = require('webpack-merge')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 
 module.exports = merge(common, {
-  mode: "production",
+  mode: 'production',
   output: {
-    filename: "[name].[contentHash].bundle.js",
-    path: path.resolve(__dirname, "dist")
+    filename: '[name].[contentHash].bundle.js',
+    path: path.resolve(__dirname, 'dist')
   },
   optimization: {
     minimizer: [
       new OptimizeCssAssetsPlugin(),
       new TerserPlugin(),
       new HtmlWebpackPlugin({
-        template: "./src/index.html",
+        template: './src/index.html',
         minify: {
           removeAttributeQuotes: true,
           collapseWhitespace: true,
@@ -28,8 +29,13 @@ module.exports = merge(common, {
     ]
   },
   plugins: [
-    new MiniCssExtractPlugin({ filename: "[name].[contentHash].css" }),
-    new CleanWebpackPlugin()
+    new MiniCssExtractPlugin({ filename: '[name].[contentHash].css' }),
+    new CleanWebpackPlugin(),
+    new FaviconsWebpackPlugin({
+      logo: './src/assets/icons/icon-310x310',
+      mode: 'webapp',
+      manifest: './src/assets/icons/manigest.json'
+    })
   ],
   module: {
     rules: [
@@ -37,10 +43,10 @@ module.exports = merge(common, {
         test: /\.scss$/,
         use: [
           MiniCssExtractPlugin.loader, //3. Extract css into files
-          "css-loader", //2. Turns css into commonjs
-          "sass-loader" //1. Turns sass into css
+          'css-loader', //2. Turns css into commonjs
+          'sass-loader' //1. Turns sass into css
         ]
       }
     ]
   }
-});
+})
